782f1a7db97e61776b92e08cd77069768ace062c
FernandoPereira218 <fernandofmp218@gmail.com>
2022-05-29 13:49:48 -0300

    teste deploy 3

--- academia_web/settings.py
+++ academia_web/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+
+django_heroku.settings(locals())
