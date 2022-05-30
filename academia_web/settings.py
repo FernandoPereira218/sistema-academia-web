e4cebca07412f37181318dfefb8b3a0f1f28c2e6
Fernando <fernandofmp218@gmail.com>
2022-05-30 13:53:52 -0300

    css em arquivo separado

--- academia_web/settings.py
+++ academia_web/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Additional locations of static files
+STATICFILES_DIRS = (
+   'templates/css/',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
